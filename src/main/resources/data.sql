d1299ab5d545594236e5c4304a2e92dc137c019f
0giri <higiri@naver.com>
2022-01-05 21:14:15 +0900

    configure infra

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user(created_by, created_date, last_modified_by, last_modified_date, email, nickname, password, type)
+values ('119.192.210.56', now(), '119.192.210.56', now(), 'higiri@naver.com', 'higiri', '1234', 'ADMIN');
+
+insert into user(created_by, created_date, last_modified_by, last_modified_date, email, nickname, password, type)
+values ('119.192.210.56', now(), '119.192.210.56', now(), 'danmi@gmail.com', 'dandan', '0829', 'BASIC');
+
+insert into user(created_by, created_date, last_modified_by, last_modified_date, email, nickname, password, type)
+values ('119.192.210.56', now(), '119.192.210.56', now(), 'dog9463@naver.com', 'dog9463', '9463', 'FREELANCER');
