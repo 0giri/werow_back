d43435e312cb63139e19f3fbe338a490dfbcc59d
0giri <higiri@naver.com>
2022-01-08 04:54:32 +0900

    Swagger 설정

--- src/main/java/com/werow/web/HomeController.java
+++ src/main/java/com/werow/web/HomeController.java
@@ -1,13 +1,15 @@
 package com.werow.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
-@RestController
+@ApiIgnore
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "ok";
+        return "redirect:/swagger-ui.html";
     }
 }
--- src/main/java/com/werow/web/user/controller/UserController.java
+++ src/main/java/com/werow/web/user/controller/UserController.java
@@ -2,16 +2,20 @@ package com.werow.web.user.controller;
 
 import com.werow.web.commons.HttpUtils;
 import com.werow.web.user.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
+@Api(tags = {"User"})
 @Controller
 @RequestMapping("/login")
 @Slf4j
@@ -21,11 +25,10 @@ public class UserController {
     private final UserService userService;
     private final HttpUtils httpUtils;
 
+    @ApiOperation(value = "카카오 연동 로그인", notes = "게스트 : 회원가입 후 로그인 처리, 회원 : 로그인 처리")
     @GetMapping("/kakao-form")
-    public String getKakaoLoginForm(HttpServletRequest request) {
-//        log.info("Client IP: {}");
+    public String getKakaoLoginForm() {
         String url = "https://kauth.kakao.com/oauth/authorize?";
-//        String hostName = httpUtils.getServerHostName(request);
 
         Map<String, Object> paramsMap = new HashMap<>();
         paramsMap.put("client_id", "e485731066d013c1fc6faaf79bfc6d04");
@@ -35,11 +38,11 @@ public class UserController {
         return "redirect:" + url + httpUtils.mapToQueryString(paramsMap);
     }
 
+    @ApiIgnore
     @GetMapping("/kakao")
     public String kakaoLogin(String code) {
         userService.kakaoLogin(code);
         return "redirect:/";
     }
-
 
 }
