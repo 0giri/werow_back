0a74ce964fe567fd0945cc0424fff8d69497bdb2
0giri <higiri@naver.com>
2022-01-13 16:40:26 +0900

    modify controller url mapping

--- src/main/java/com/werow/web/controller/OAuthController.java
+++ src/main/java/com/werow/web/controller/OAuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/oauth")
+@RequestMapping("/api/oauth")
 public class OAuthController {
 
     private final MemberRepository memberRepository;
